983551d6bf7a20eeef5fcb46194f776f47fa71e6
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-06-25 12:53:36 -0400

    create nes lesson input similar to a dto for creating lessons

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Lesson } from './lesson.entity';
 import { Repository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
+import { CreateLessonInput } from './lesson.input';
 
 @Injectable()
 export class LessonService {
@@ -10,7 +11,10 @@ export class LessonService {
     @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
   ) {}
 
-  public async createLesson(name, startDate, endDate): Promise<Lesson> {
+  public async createLesson(
+    createLessonInput: CreateLessonInput,
+  ): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
