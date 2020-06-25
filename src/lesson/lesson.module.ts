b2b90060e0bc8fae4df20be9f2179bbe06e105b5
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-06-25 11:56:21 -0400

    add createLesson mutation to add new lessons to mongodb

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { LessonResolver } from './lesson.resolver';
+import { LessonService } from './lesson.service';
+import { Lesson } from './lesson.entity';
 
 @Module({
-  providers: [LessonResolver],
+  imports: [TypeOrmModule.forFeature([Lesson])],
+  providers: [LessonResolver, LessonService],
 })
 export class LessonModule {}
